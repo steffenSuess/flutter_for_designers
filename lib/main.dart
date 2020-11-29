c6e62cdc483859407c6e975a6749cabdfa5ac875
Steffen Suess <steffen.suess@outlook.de>
2020-11-29 13:38:40 +0100

    -implemented components

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_designers/model/sidebar.dart';
+
+import 'components/sidebar_row.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,43 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Row(
-            children: [
-              Container(
-                  width: 42.0,
-                  height: 42.0,
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14.0),
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color(0xFF00AEFF),
-                        Color(0xFF0076FF),
-                      ],
-                    ),
-                  ),
-                  child: Icon(
-                    Icons.home,
-                    color: Colors.white,
-                  )),
-              SizedBox(
-                width: 12.0,
-              ),
-              Container(
-                child: Text(
-                  'Home',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xFF242629),
-                  ),
-                ),
-              ),
-            ],
+          child: SidebarRow(
+            item: sidebarItem[0],
           ),
         ),
       ),
