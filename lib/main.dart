41f6d09f6c3f82756da8c628ad5bec4d30668242
Steffen Suess <steffen.suess@outlook.de>
2020-11-30 13:14:43 +0100

    -refactored main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_for_designers/constants.dart';
-import 'package:flutter_for_designers/model/course.dart';
 
 import 'components/home_screen_navbar.dart';
+import 'components/lists/explore_course_list.dart';
 import 'components/lists/recent_course_list.dart';
 
 void main() {
@@ -58,81 +58,6 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 ExploreCourseList(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ExploreCourseList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: exploreCourses.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(left: index == 0 ? 20.0 : 0.0),
-            child: ExploreCourseCard(course: exploreCourses[index]),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ExploreCourseCard extends StatelessWidget {
-  ExploreCourseCard({@required this.course});
-  final Course course;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 20.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(41.0),
-        child: Container(
-          height: 120.0,
-          width: 280.0,
-          decoration: BoxDecoration(gradient: course.background),
-          child: Padding(
-            padding: EdgeInsets.only(left: 32.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        course.courseSubtitle,
-                        style: kCardSubtitleStyle,
-                      ),
-                      SizedBox(
-                        height: 6.0,
-                      ),
-                      Text(
-                        course.courseTitle,
-                        style: kCardTitleStyle,
-                      )
-                    ],
-                  ),
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Image.asset(
-                      'asset/illustrations/${course.illustration}',
-                      fit: BoxFit.cover,
-                      height: 100.0,
-                    ),
-                  ],
-                )
               ],
             ),
           ),
