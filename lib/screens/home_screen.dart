62176bcfdd0370161681d5e39e0eff8de78ee8c0
Steffen Suess <steffen.suess@outlook.de>
2020-12-02 10:17:01 +0100

    -implemented slidingup panel for continue watching

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_for_designers/components/lists/recent_course_list.dart';
 import 'package:flutter_for_designers/screens/sidebar_screen.dart';
 
 import '../constants.dart';
+import 'continue_watching_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -104,6 +105,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 ],
               ),
             ),
+            ContinueWatchingScreen(),
             IgnorePointer(
               ignoring: sidebarHidden,
               child: Stack(
